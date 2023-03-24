4bf605e6e747cfcca64a0315f2a815f34285fa08
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-03-24 22:04:05 +0200

    gh-pages were installed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import { BookEditPage } from './pages/BookEditPage';
